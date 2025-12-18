c6d05417ab25b67cf892074c1fd1a32a1002311d
clovan Leroukx <mbengclovis@yahoo.fr>
2025-12-18 14:23:07 +0100

    adding data and model files

--- src/banking_transaction_api/main.py
+++ src/banking_transaction_api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from src.banking_transaction_api.database import get_all_transactions
 app = FastAPI(
     title="Banking Transactions API",
     version="1.0.0"
@@ -17,3 +17,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
+
+@app.get("/transactions")
+def read_transactions():
+    return get_all_transactions()
