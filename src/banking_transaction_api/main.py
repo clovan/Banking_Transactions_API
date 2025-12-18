05d21b45aa29f8008daf175ca8443c3a740c566b
clovan Leroukx <mbengclovis@yahoo.fr>
2025-12-18 12:51:16 +0100

    Initialisation du projet avec Main et init

---
+++ src/banking_transaction_api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Banking Transactions API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def read_root():
+    return {
+        "status": "online",
+        "message": "Bienvenue sur l'API Bancaire",
+        "team": ["Clovis", "Irmeline", "Milaine", "Amina"],
+        "documentation": "/docs"
+    }
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
