f2f0db4ab9c24dad10ef3bfd1bb8a6da7f719ed2
clovan Leroukx <mbengclovis@yahoo.fr>
2025-12-21 11:30:24 +0100

    ma premiere route: Transaction

--- src/banking_transaction_api/main.py
+++ src/banking_transaction_api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from src.banking_transaction_api.database import get_all_transactions
-app = FastAPI(
-    title="Banking Transactions API",
-    version="1.0.0"
-)
+from src.banking_transaction_api.routers.transactions import router as trans_router
+
+app = FastAPI(title="Banking API")
+
+app.include_router(trans_router)
 
 @app.get("/")
-def read_root():
-    return {"status": "success", "message": "API de Transactions Bancaires opérationnelle"}
+def health_check():
+    return {"status": "online"}
---
+++ src/banking_transaction_api/routers/transactions.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Query, HTTPException
+from src.banking_transaction_api.services.transaction_service import TransactionService
+
+router = APIRouter(prefix="/api/transactions", tags=["Transactions"])
+service = TransactionService()
+
+
+@router.get("/")
+def list_transactions(
+        page: int = Query(1, ge=1),
+        limit: int = Query(20, ge=1, le=100),
+        type: str = None,
+        isFraud: int = Query(None, ge=0, le=1),
+        min_amount: float = None,
+        max_amount: float = None
+):
+    df = service.get_all()
+    if df.empty:
+        raise HTTPException(status_code=404, detail="Fichier de données introuvable ou vide")
+
+    df_filtered = service.filter_transactions(df, type, isFraud, min_amount, max_amount)
+
+    start = (page - 1) * limit
+    results = df_filtered.iloc[start: start + limit].to_dict(orient="records")
+
+    return {
+        "page": page,
+        "limit": limit,
+        "total_results": len(df_filtered),
+        "transactions": results
+    }
